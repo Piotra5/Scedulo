3048c258c09f96f9c9cd9c82f83a9067155e906f
Piotra5 <pilch.piotr5@gmail.com>
2019-11-14 13:49:54 +0100

    Changed login controller to return user id.

--- Scedulo/Scedulo/Server/Controllers/Base/LoginController.cs
+++ Scedulo/Scedulo/Server/Controllers/Base/LoginController.cs
@@ -20,25 +20,28 @@ namespace Scedulo.Server.Controllers.Base
     {
         private readonly IConfiguration _configuration;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
         public LoginController(IConfiguration configuration,
-                               SignInManager<ApplicationUser> signInManager)
+                               SignInManager<ApplicationUser> signInManager,
+                               UserManager<ApplicationUser> userManager)
         {
             _configuration = configuration;
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpPost]
         public async Task<IActionResult> Login([FromBody] LoginModel login)
         {
             var result = await _signInManager.PasswordSignInAsync(login.Email, login.Password, false, false);
-
             if (!result.Succeeded) return BadRequest(new LoginResult { Successful = false, Error = "Username and password are invalid." });
-
+            var user = await _userManager.FindByEmailAsync(login.Email);
             var claims = new[]
             {
-            new Claim(ClaimTypes.Name, login.Email)
-        };
+                new Claim(ClaimTypes.Name, login.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSecurityKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
@@ -52,7 +55,7 @@ namespace Scedulo.Server.Controllers.Base
                 signingCredentials: creds
             );
 
-            return Ok(new LoginResult { Successful = true, Token = new JwtSecurityTokenHandler().WriteToken(token) });
+            return Ok(new LoginResult { UserId = user.Id, Successful = true, Token = new JwtSecurityTokenHandler().WriteToken(token) });
         }
     }
 }
--- Scedulo/Scedulo/Shared/Models/Users/LoginResult.cs
+++ Scedulo/Scedulo/Shared/Models/Users/LoginResult.cs
@@ -6,7 +6,9 @@ namespace Scedulo.Shared.Models.Users
 {
     public class LoginResult
     {
+        public string UserId { get; set; }
         public bool Successful { get; set; }
+        public int MyProperty { get; set; }
         public string Error { get; set; }
         public string Token { get; set; }
     }
